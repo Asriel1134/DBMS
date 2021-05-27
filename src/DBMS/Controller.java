1c05bbd4ff40b9c57d3b90d81b4db04a0caf496c
Asriel1134 <1134175939@qq.com>
2021-05-27 14:52:42 +0800

    ICON

--- src/DBMS/Controller.java
+++ src/DBMS/Controller.java
@@ -1,4 +1,4 @@
-package sample;
+package DBMS;
 
 public class Controller {
 }
--- src/DBMS/Main.java
+++ src/DBMS/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package DBMS;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -7,17 +7,15 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Sign in");
+        primaryStage.setScene(new Scene(root, 400, 220));
         primaryStage.show();
-    }
-
-
-    public static void main(String[] args) {
-        launch(args);
     }
 }
